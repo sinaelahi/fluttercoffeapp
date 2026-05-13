c0bd48af8847549e1af237ebb0d2d113d9c21c87
Sina <sinaelahu@gmail.com>
2026-05-13 20:12:53 +0330

    new file add

---
+++ feature/CafeSection/module/cafe_model.dart
@@ -0,0 +1,8 @@
+class CafeModel {
+  String coffeeurl;
+  String coffename;
+  String desc;
+  String price;
+
+  CafeModel({required this.coffeeurl,required this.coffename,required this.desc,required this.price});
+}
