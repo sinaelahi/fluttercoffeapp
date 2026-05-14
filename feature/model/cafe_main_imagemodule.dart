3f9bb9ea909dd3e49f8f2c14d83d0c2f550af4c0
Sina <sinaelahu@gmail.com>
2026-05-14 13:11:25 +0330

    model file moved

---
+++ feature/model/cafe_main_imagemodule.dart
@@ -0,0 +1,61 @@
+
+
+import 'package:sahand/coffeapp/fluttercoffeapp/feature/CafeSection/model/cafe_model.dart';
+
+//coffe models
+
+class CafeMainImagemodule {
+ List<CafeModel> getdata(){
+   List<CafeModel> coffeeimages = [];
+  
+  CafeModel cafemodel1 = CafeModel(
+    coffeeurl: 'assets/images/2.png',
+    coffename: "Mocha",
+    desc: "Deep Foam",
+    price: "5.53");
+  CafeModel cafemodel2 = CafeModel(
+    coffeeurl: 'assets/images/3.png',
+    coffename: "Flat White",
+    desc: "Deep Foam",
+    price: "4.89");
+  CafeModel cafemodel3 = CafeModel(
+    coffeeurl: 'assets/images/4.png',
+    coffename: "Caffe Panna",
+    desc: "Ice/Hot",
+    price: "4.53");
+  CafeModel cafemodel4 = CafeModel(
+    coffeeurl: 'assets/images/5.png',
+    coffename: "Mocha",
+    desc: "Ice/Hot",
+    price: "6.53");
+  CafeModel cafemodel5 = CafeModel(
+    coffeeurl: 'assets/images/5.png',
+    coffename: "Milk",
+    desc: "Ice/Hot",
+    price: "6.53");
+  CafeModel cafemodel6 = CafeModel(
+    coffeeurl: 'assets/images/2.png',
+    coffename: "Choclate",
+    desc: "Deep Foam",
+    price: "6.53");
+  CafeModel cafemodel7 = CafeModel(
+    coffeeurl: 'assets/images/3.png',
+    coffename: "Mocha",
+    desc: "Deep Foam",
+    price: "6.53");
+
+  coffeeimages.add(cafemodel1);
+  coffeeimages.add(cafemodel2);
+  coffeeimages.add(cafemodel3);
+  coffeeimages.add(cafemodel4);
+  coffeeimages.add(cafemodel5);
+  coffeeimages.add(cafemodel6);
+  coffeeimages.add(cafemodel7);
+
+
+  return coffeeimages;
+ }
+    
+    
+      
+}
